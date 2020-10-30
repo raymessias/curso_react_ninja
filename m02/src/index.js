c4f1bfc5485d04925bed18d153305e865e5ea3b2
Raynon Messias <raymessias@gmail.com>
2020-10-30 02:12:07 -0300

    commit: Utilizando o commonJs

--- m02/src/index.js
+++ m02/src/index.js
@@ -1,1 +1,10 @@
-console.log('Webpack está funcionando')
+'use strict'
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+var Title = require('./app')
+
+ReactDOM.render(
+  React.createElement(Title),
+  document.querySelector('[data-js="app"]')
+)
