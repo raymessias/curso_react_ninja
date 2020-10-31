ffde25ebc5ad816f240d6ab3cb67ec5ec2624dfc
Raynon Messias <raymessias@gmail.com>
2020-10-31 13:20:10 -0300

    commit: setState Assíncrono

--- m02/src/app.js
+++ m02/src/app.js
@@ -6,26 +6,28 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Valor inicial',
-      checked: false
+      checked: false,
+      showContent: false
     }
   }
 
   render () {
     return (
       <div>
-        <form>
-          <input type='text' value={this.state.value} onChange={(e) => {
-            this.setState({ value: e.target.value })
-          }}
-          />
-          <label>
-            <input type='checkbox' value='mycheck' checked={this.state.checked}
-              onChange={(e) => this.setState({ checked: !this.state.checked })}
-            />
-            Checkbox
-          </label>
-        </form>
+        <label>
+          <input
+            type='checkbox'
+            checked={this.state.checked}
+            onChange={() => {
+              this.setState({ checked: !this.state.checked }, () => {
+                this.setState({
+                  showContent: this.state.checked
+                })
+              })
+            }}
+          /> Mostrar conteudo
+        </label>
+        {this.state.showContent && <div>Olha eu aqui!</div>}
       </div>
     )
   }
